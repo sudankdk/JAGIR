7a047b96edfe0b034cd8d616da41b9fea15b8946
sudankdk <sudankhadka345@gmail.com>
2025-03-10 10:05:23 +0000

    migration

--- backend jagir/base/settings.py
+++ backend jagir/base/settings.py
@@ -155,7 +155,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5173",
+    "http://localhost:5173","http://127.0.0.1:5173"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -167,9 +167,7 @@ MEDIA_ROOT= os.path.join(BASE_DIR,'media')
 
 
 INTERNAL_IPS = [
- 
-    "127.0.0.1",
-  
+    "127.0.0.1", 
 ]
 
 DEBUG_TOOLBAR_PANELS = [
